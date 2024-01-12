a417500cf0d6450413e2aba8faadd42e831d60a2
Arnab Roy <arnab.roy@xeneta.com>
2024-01-12 18:11:47 +0100

    Update highly irritating <C-k> remap

--- dotfiles/nvim/after/plugin/lsp.lua
+++ dotfiles/nvim/after/plugin/lsp.lua
@@ -33,7 +33,7 @@ local on_attach = function(_, bufnr)
 
 	-- See `:help K` for why this keymap
 	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+	nmap("<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
 
 	-- Lesser used LSP functionality
 	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
