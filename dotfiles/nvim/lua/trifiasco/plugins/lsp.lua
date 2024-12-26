6c8712279aa4a3d5507cfccbc57b7c3eb2d919a4
Arnab Roy <trifiasco@gmail.com>
2024-12-26 21:22:37 +0100

    Remove clang and go from lsp servers

--- dotfiles/nvim/lua/trifiasco/plugins/lsp.lua
+++ dotfiles/nvim/lua/trifiasco/plugins/lsp.lua
@@ -58,18 +58,18 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-	clangd = {},
-    gopls = {
-        filetypes = { "go", "gomod" },
-        settings = {
-            gopls = {
-                analyses = {
-                    unusedparams = true,
-                },
-                staticcheck = true,
-            },
-        },
-    },
+	-- clangd = {},
+	--    gopls = {
+	--        filetypes = { "go", "gomod" },
+	--        settings = {
+	--            gopls = {
+	--                analyses = {
+	--                    unusedparams = true,
+	--                },
+	--                staticcheck = true,
+	--            },
+	--        },
+	--    },
 	pyright = {
 		single_file_support = true,
 		settings = {
