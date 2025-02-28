a0b3b184dd82360dc88cffd18abdbafc74b45659
Arnab Roy <trifiasco@gmail.com>
2025-02-28 17:11:06 +0100

    Add latex plugin

--- dotfiles/nvim/lua/trifiasco/plugins/init.lua
+++ dotfiles/nvim/lua/trifiasco/plugins/init.lua
@@ -127,6 +127,25 @@ return {
         init = function()
             -- VimTeX configuration goes here, e.g.
             vim.g.vimtex_view_method = "zathura"
+
+            -- xelatex with latexmk for better font support
+            vim.g.vimtex_compiler_latexmk = {
+              executable = 'latexmk',
+              options = {
+                '-pdf',
+                '-pdflatex=xelatex',
+                '-file-line-error',
+                '-synctex=1',
+                '-interaction=nonstopmode',
+              },
+            }
+            vim.g.vimtex_compiler_latexmk_engines = {
+              _        = '-pdf',
+              pdflatex = '-pdf',
+              xelatex  = '-xelatex',
+              lualatex = '-lualatex',
+            }
+            vim.g.vimtex_compiler_engine = 'xelatex'
         end
     },
     {"github/copilot.vim",}
