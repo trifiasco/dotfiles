784d8f65e7f1ae436f7e4a714cddcb38ec36966d
Arnab Roy <trifiasco@gmail.com>
2025-06-10 00:05:03 +0200

    Add copilot chat plugin

---
+++ dotfiles/nvim/lua/trifiasco/plugins/copilot-chat.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    config = function()
+            require("CopilotChat").setup()
+    end,
+    build = "make tiktoken", -- Only on MacOS or Linux
+    -- opts = {
+    --   -- See Configuration section for options
+    -- },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
