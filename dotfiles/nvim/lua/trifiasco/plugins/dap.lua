6a9292b9431b5b7fd8adaed84244e4d3219b1a34
Arnab Roy <trifiasco@gmail.com>
2026-02-16 19:57:10 +0100

    Fix startup time by lazy loading plugins and optimizing keymaps
    - remove all eager require calls
    - move keymaps to plugin specs under keys property
    - rewrote dap config to be lazy loaded and only load when needed
    - move telescope keymaps with cmd and keys lazy loading

--- dotfiles/nvim/lua/trifiasco/plugins/dap.lua
+++ dotfiles/nvim/lua/trifiasco/plugins/dap.lua
@@ -1,71 +1,77 @@
-local dap = require('dap')
-local dapui = require('dapui')
-
--- Initialize DAP UI
-dapui.setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  layouts = {
-    {
-      elements = {
-        'scopes',
-        'breakpoints',
-        'stacks',
-        'watches',
-      },
-      size = 40,
-      position = 'left',
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-telescope/telescope-dap.nvim",
     },
-    {
-      elements = {
-        'repl',
-        'console',
-      },
-      size = 10,
-      position = 'bottom',
+    keys = {
+        { "<F5>", function() require("dap").continue() end, desc = "DAP Continue" },
+        { "<F10>", function() require("dap").step_over() end, desc = "DAP Step Over" },
+        { "<F11>", function() require("dap").step_into() end, desc = "DAP Step Into" },
+        { "<F12>", function() require("dap").step_out() end, desc = "DAP Step Out" },
+        { "<localleader>b", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>B", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Conditional Breakpoint" },
+        { "<leader>dr", function() require("dap").repl.open() end, desc = "DAP REPL" },
+        { "<leader>dl", function() require("dap").run_last() end, desc = "DAP Run Last" },
     },
-  },
-})
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
 
--- Enable virtual text
-require('nvim-dap-virtual-text').setup()
+        -- Initialize DAP UI
+        dapui.setup({
+            icons = { expanded = "▾", collapsed = "▸" },
+            mappings = {
+                expand = { "<CR>", "<2-LeftMouse>" },
+                open = "o",
+                remove = "d",
+                edit = "e",
+                repl = "r",
+                toggle = "t",
+            },
+            layouts = {
+                {
+                    elements = {
+                        "scopes",
+                        "breakpoints",
+                        "stacks",
+                        "watches",
+                    },
+                    size = 40,
+                    position = "left",
+                },
+                {
+                    elements = {
+                        "repl",
+                        "console",
+                    },
+                    size = 10,
+                    position = "bottom",
+                },
+            },
+        })
 
--- Add Telescope integration
-require('telescope').load_extension('dap')
+        -- Enable virtual text
+        require("nvim-dap-virtual-text").setup()
 
--- dap-python specific configuration
-local dap_python = require('dap-python')
-dap_python.test_runner = 'pytest'
+        -- Add Telescope integration
+        require("telescope").load_extension("dap")
 
--- Add keymappings
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<F5>', dap.continue, opts)
-vim.keymap.set('n', '<F10>', dap.step_over, opts)
-vim.keymap.set('n', '<F11>', dap.step_into, opts)
-vim.keymap.set('n', '<F12>', dap.step_out, opts)
-vim.keymap.set('n', '<localleader>b', dap.toggle_breakpoint, opts)
-vim.keymap.set('n', '<leader>B', function()
-  dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
-end, opts)
-vim.keymap.set('n', '<leader>dr', dap.repl.open, opts)
-vim.keymap.set('n', '<leader>dl', dap.run_last, opts)
+        -- dap-python specific configuration
+        local dap_python = require("dap-python")
+        dap_python.test_runner = "pytest"
 
--- Automatically open/close DAP UI
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
-
-return {}
+        -- Automatically open/close DAP UI
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+        end
+    end,
+}
